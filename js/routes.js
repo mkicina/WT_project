94992d0eaa2864249bd9648bf3d0ee9e41f480e7
Martin Kičina <martin.kicina@student.tuke.sk>
2020-04-30 11:59:38 +0200

    done bez strankovania

--- js/routes.js
+++ js/routes.js
@@ -1,12 +1,12 @@
-//an array, defining the routes
+
 export default[
 
     {
-        //the part after '#' in the url (so-called fragment):
+
         hash:"welcome",
-        ///id of the target html element:
+
         target:"router-view",
-        //the function that returns content to be rendered to the target html element:
+
         getTemplate:(targetElm) =>
             document.getElementById(targetElm).innerHTML = document.getElementById("template-welcome").innerHTML
 
@@ -18,11 +18,22 @@ export default[
         getTemplate: fetchAndDisplayArticles
     },
 
-
     {
         hash:"opinions",
         target:"router-view",
         getTemplate: createHtml4opinions
+    },
+
+    {
+        hash:"article",
+        target:"router-view",
+        getTemplate: fetchAndDisplayArticleDetail
+    },
+
+    {
+        hash:"artEdit",
+        target:"router-view",
+        getTemplate: editArticle
     },
     {
         hash:"addOpinion",
@@ -32,38 +43,9 @@ export default[
     }
 ];
 
-function createHtml4Main(targetElm, current,totalCount){
 
-    current=parseInt(current);
-    totalCount=parseInt(totalCount);
-    const data4rendering={
-        currPage:current,
-        pageCount:totalCount
-    };
-
-
-
-    if(current>1){
-        data4rendering.prevPage=current-1;
-    }
-
-    if(current<totalCount){
-        data4rendering.nextPage=current+1;
-    }
-
-    document.getElementById(targetElm).innerHTML = Mustache.render(
-        document.getElementById("template-main").innerHTML,
-        data4rendering
-    );
-
-
-       /* return `<h1>Main Content</h1>
-                ${current} <br>
-                ${totalCount} <br>
-                ${JSON.stringify(data4rendering)}
-                `;*/
-
-}
+const urlBase = "https://wt.kpi.fei.tuke.sk/api";
+const articlesPerPage = 20;
 
 function createHtml4opinions(targetElm){
     const opinionsFromStorage=localStorage.myTreesComments;
@@ -84,98 +66,20 @@ function createHtml4opinions(targetElm){
 }
 
 
-function fetchAndDisplayArticles(targetElm, current, totalCount){
-    const url = "https://wt.kpi.fei.tuke.sk/api/article";
-    let articleList =[];
-    if(current === "0"){
-        current=parseInt(current) + 1;
-        totalCount=parseInt(totalCount) + 1;
-    } else {
-        current=parseInt(current);
-        totalCount=parseInt(totalCount);
-    }
-    const data4rendering={
-        currPage: current,
-        pageCount: totalCount,
-        offset: (current-1) * 20
-    };
-    if(current>1){
-        data4rendering.prevPage=current-1;
+function fetchAndDisplayArticles(targetElm, offsetFromHash, totalCountFromHash){
+
+    const offset=Number(offsetFromHash);
+    const totalCount=Number(totalCountFromHash);
+
+    let urlQuery = "";
+
+    if (offset && totalCount){
+        urlQuery=`?offset=${offset}&max=${articlesPerPage}`;
+    }else{
+        urlQuery=`?max=${articlesPerPage}`;
     }
 
-    if(current<totalCount){
-        data4rendering.nextPage=current+1;
-    }
-
-    fetch(url+ "/?max=20" + "&offset=" + data4rendering.offset)
-        .then(response =>{
-            if(response.ok){
-                return response.json();
-            }else{ //if we get server error
-                return Promise.reject(new Error(`Server answered with ${response.status}: ${response.statusText}.`));
-            }
-        })
-        .then(responseJSON => {
-            articleList=responseJSON.articles;
-            return Promise.resolve();
-        })
-        .then( ()=> {
-            let cntRequests = articleList.map(
-                article => fetch(`${url}/${article.id}`)
-            );
-            return Promise.all(cntRequests);
-        })
-        .then(responses =>{
-            let failed="";
-            for(let response of responses) {
-                if(!response.ok) failed+=response.url+" ";
-            }
-            if(failed===""){
-                return responses;
-            }else{
-                return Promise.reject(new Error(`Failed to access the content of the articles with urls ${failed}.`));
-            }
-        })
-        .then(responses => Promise.all(responses.map(resp => resp.json())))
-        .then(articles => {
-            articles.forEach((article,index) =>{
-                articleList[index].content=article.content;
-            });
-
-            return Promise.resolve();
-        })
-        .then( () =>{
-            let data = [];
-            data.articles = articleList;
-            data.currPage = data4rendering.currPage;
-            data.pageCount = data4rendering.pageCount;
-            data.prevPage = data4rendering.prevPage;
-            data.nextPage = data4rendering.nextPage;
-            document.getElementById(targetElm).innerHTML =Mustache.render(document.getElementById("template-articles").innerHTML, data);
-        })
-        .catch (error => { ////here we process all the failed promises
-            const errMsgObj = {errMessage:error};
-            document.getElementById(targetElm).innerHTML =
-                Mustache.render(
-                    document.getElementById("template-articles-error").innerHTML,
-                    errMsgObj
-                );
-        });
-
-    console.log("articleList: " + articleList);
-}
-/*
-function fetchAndDisplayArticles(targetElm){
-
-    const url = "http://wt.kpi.fei.tuke.sk/api/article/?max=20&offset=0";
-
-
-    const articlesElm = document.getElementById("articles");
-    const errorElm = document.getElementById("error");
-
-    let articleList =[];
-
-
+    const url = `${urlBase}/article${urlQuery}`;
 
     fetch(url)
         .then(response =>{
@@ -186,41 +90,93 @@ function fetchAndDisplayArticles(targetElm){
             }
         })
         .then(responseJSON => {
-            articleList=responseJSON.articles;
-            return Promise.resolve();
-        })
-        .then( ()=> {
-            let cntRequests = articleList.map(
-                article => fetch(`${"http://wt.kpi.fei.tuke.sk/api/article"}/${article.id}/${"offset=1"}`)
+            addArtDetailLink2ResponseJson(responseJSON);
+            document.getElementById(targetElm).innerHTML =
+                Mustache.render(
+                    document.getElementById("template-articles").innerHTML,
+                    responseJSON
                 );
-            return Promise.all(cntRequests);
         })
-        .then(responses =>{
-            let failed="";
-            for(let response of responses) {
-                if(!response.ok) failed+=response.url+" ";
+        .catch (error => { ////here we process all the failed promises
+            const errMsgObj = {errMessage:error};
+            document.getElementById(targetElm).innerHTML =
+                Mustache.render(
+                    document.getElementById("template-articles-error").innerHTML,
+                    errMsgObj
+                );
+        });
+}
+
+function addArtDetailLink2ResponseJson(responseJSON){
+    responseJSON.articles =
+        responseJSON.articles.map(
+            article =>(
+                {
+                    ...article,
+                    detailLink:`#article/${article.id}/${responseJSON.meta.offset}/${responseJSON.meta.totalCount}`
+                }
+            )
+        );
+}
+
+function fetchAndDisplayArticleDetail(targetElm, artIdFromHash, offsetFromHash, totalCountFromHash) {
+    fetchAndProcessArticle(...arguments,false);
+}
+
+
+/**
+ * Gets an article record from a server and processes it to html according to the value of the forEdit parameter.
+ * Assumes existence of the urlBase global variable with the base of the server url (e.g. "https://wt.kpi.fei.tuke.sk/api"),
+ * availability of the Mustache.render() function and Mustache templates with id="template-article" (if forEdit=false)
+ * and id="template-article-form" (if forEdit=true).
+ * @param targetElm - element to which the acquired article record will be rendered using the corresponding template
+ * @param artIdFromHash - id of the article to be acquired
+ * @param offsetFromHash - current offset of the article list display to which the user should return
+ * @param totalCountFromHash - total number of articles on the server
+ * @param forEdit - if false, the function renders the article to HTML using the template-article for display.
+ *                  If true, it renders using template-article-form for editing.
+ */
+function fetchAndProcessArticle(targetElm, artIdFromHash, offsetFromHash, totalCountFromHash,forEdit) {
+    const url = `${urlBase}/article/${artIdFromHash}`;
+
+    fetch(url)
+        .then(response =>{
+            if(response.ok){
+                return response.json();
+            }else{ //if we get server error
+                return Promise.reject(new Error(`Server answered with ${response.status}: ${response.statusText}.`));
             }
-            if(failed===""){
-                return responses;
+        })
+        .then(responseJSON => {
+
+
+            if(forEdit){
+                responseJSON.formTitle="Article Edit";
+                responseJSON.formSubmitCall =
+                    `processArtEditFrmData(event,${artIdFromHash},${offsetFromHash},${totalCountFromHash},'${urlBase}')`;
+                responseJSON.submitBtTitle="Save article";
+                responseJSON.urlBase=urlBase;
+
+                responseJSON.backLink=`#article/${artIdFromHash}/${offsetFromHash}/${totalCountFromHash}`;
+
+                document.getElementById(targetElm).innerHTML =
+                    Mustache.render(
+                        document.getElementById("template-article-form").innerHTML,
+                        responseJSON
+                    );
             }else{
-                return Promise.reject(new Error(`Failed to access the content of the articles with urls ${failed}.`));
+
+                responseJSON.backLink=`#articles/${offsetFromHash}/${totalCountFromHash}`;
+                responseJSON.editLink=`#artEdit/${responseJSON.id}/${offsetFromHash}/${totalCountFromHash}`;
+                responseJSON.deleteLink=`#artDelete/${responseJSON.id}/${offsetFromHash}/${totalCountFromHash}`;
+
+                document.getElementById(targetElm).innerHTML =
+                    Mustache.render(
+                        document.getElementById("template-article").innerHTML,
+                        responseJSON
+                    );
             }
-        })
-        .then(responses => Promise.all(responses.map(resp => resp.json())))
-        .then(articles => {
-            articles.forEach((article,index) =>{
-                articleList[index].content=article.content;
-            });
 
-            return Promise.resolve();
-        })
-        .then( () =>{
-
-            let data = [];
-            data.articles = articleList;
-
-
-            document.getElementById(targetElm).innerHTML =Mustache.render(document.getElementById("template-articles").innerHTML, data);
         })
         .catch (error => { ////here we process all the failed promises
             const errMsgObj = {errMessage:error};
@@ -232,6 +188,7 @@ function fetchAndDisplayArticles(targetElm){
         });
 
 }
-*/
 
-
+function editArticle(targetElm, artIdFromHash, offsetFromHash, totalCountFromHash) {
+    fetchAndProcessArticle(...arguments,true);
+}
